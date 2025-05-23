2bb9c66858c1923bdfb6318a3e4fd2a914ebc5fa
RajaRajeshwari1812 <rajarajeshwarim18@gmail.com>
2025-05-23 11:53:08 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const techStack = ['React', 'Node.js', 'Express.js'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
+      <h2>My Tech Stack</h2>
+      <ul>
+        {techStack.map((tech, index) => (
+          <li key={index}>{tech}</li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
